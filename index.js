cf0ca4c343a9ed7bd83870cf997e8ae6775c8a6e
FStriewski <fstriewski@outlook.com>
2018-03-02 10:19:24 +0100

    Add migration by using the starter kit as blueprint

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postg
 const app = express()
 
 app.use(cors())
+
+/* More or less equal to
+app.use(function(req, res, next) {
+res.header('Access-Control-Allow-Origin', '*')
+res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+next()
+})
+*/
+
+
 app.use(bodyParser.json())
 
 app.listen(4001, () => console.log('Express API listening on port 4001'))
---
+++ migrations/migrate-create-products.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Products', {
+
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Products');
+  }
+};
