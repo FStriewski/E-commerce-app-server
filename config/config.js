38df761e868e4c2e9e61fc7f4e5beee6b1617667
FStriewski <fstriewski@outlook.com>
2018-03-02 13:24:31 +0100

    Add production db

--- config/config.js
+++ config/config.js
@@ -7,6 +7,6 @@
     "dialect": "postgres"
   },
   "production": {
-    "use_env_variable": "DATABASE_URL"
+    "use_env_variable": "postgres://tablqgiecdpmke:d5fb4f4e9a1cf2a0b27c29d0624cd7a18c9bc354141191e9d15166fcb9fd4feb@ec2-54-204-46-60.compute-1.amazonaws.com:5432/deit64pvftvbpb"
   }
 }
